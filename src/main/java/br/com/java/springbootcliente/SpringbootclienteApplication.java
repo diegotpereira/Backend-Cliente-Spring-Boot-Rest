d3c4cc44a5e849f3bae0ce43280a1ba96b83d804
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-09-01 08:21:35 -0300

    first commit

---
+++ src/main/java/br/com/java/springbootcliente/SpringbootclienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.springbootcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/springbootcliente/SpringbootclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.springbootcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
